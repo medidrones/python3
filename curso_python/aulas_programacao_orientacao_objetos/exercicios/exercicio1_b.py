e5eaccd75794b9a1e52abeec2ef19f74708b8480
Jorge Medina <medidrones@gmail.com>
2024-02-11 15:30:25 -0300

    Finalizado Seção de Programação Orientada a Objetos

---
+++ curso_python/aulas_programacao_orientacao_objetos/exercicios/exercicio1_b.py
@@ -0,0 +1,14 @@
+import json
+
+from exercicio1_a import CAMINHO_ARQUIVO, Pessoa, fazer_dump
+
+# fazer_dump()
+
+with open(CAMINHO_ARQUIVO, 'r') as arquivo:
+    pessoas = json.load(arquivo)
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+    p3 = Pessoa(**pessoas[2])
+
+    print(p1.nome, p1.idade)
+    print(p2.nome, p2.idade)
